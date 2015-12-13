b1c8abe5617c8aeb27ee2011a4c5fe15d2fc0dd5
Josef Patoprsty <seppi@josefnpat.com>
2015-12-13 17:24:42 -0600

    Adding working stuff for arjan

--- src/main.lua
+++ src/main.lua
@@ -13,6 +13,20 @@ lib = {
   ilovetexpack = require "ilovetexpack",
 }
 
+fonts = {
+  default = love.graphics.newFont("IndieFlower.ttf",24)
+}
+
+colors = {
+  text_background = {0,0,0,191},
+  text = {255,255,255},
+  reset = {255,255,255},
+  death = {255,0,0},
+}
+
+atlas = lib.ilovetexpack.render("atlas")
+atlas:getImage():setFilter("nearest","nearest")
+
 function love.load()
   hump.gamestate.registerEvents()
   hump.gamestate.switch(gamestates.game)
--- src/state_game.lua
+++ src/state_game.lua
@@ -1,8 +1,11 @@
-local game = {}
+local game = {
+  bind = {
+    feed = function() return love.keyboard.isDown("a") end,
+    pet = function() return love.keyboard.isDown("l") end,
+  }
+}
 
 function game:init()
-  self.atlas = lib.ilovetexpack.render("atlas")
-  self.atlas:getImage():setFilter("nearest","nearest")
   self.background = love.graphics.newImage("background.png")
 
   self.paws = {"normal"}
@@ -11,17 +14,22 @@ function game:init()
   self.food = {"asparagus","bag","beer","boot","bowl","can","fish","goebbels",
     "pizza","porcupine","sonic","squirrel"}
 
+  self.debug = false
+
+  self.cpaws = "paws_"..self.paws[math.random(#self.paws)]..".png"
 end
 
-function game:randomFeatures()
-  self.cpaws = "paws_"..self.paws[math.random(#self.paws)]..".png"
-  self.ceyes = "eyes_"..self.eyes[math.random(#self.eyes)]..".png"
-  self.cmouth = "mouth_normal.png"
+function game:feedCat()
   self.cfood = "food_"..self.food[math.random(#self.food)]..".png"
-  self.csize = (self.csize + 0.01)%1
+  self.csize = math.min(1,self.csize + 0.025)
 end
 
 function game:enter()
+
+  self.dead = false
+  self.death_fade = 0
+
+  self.cfood = "food_bowl.png"
   self.food_offset = 0
   self.food_direction = 1
 
@@ -29,38 +37,89 @@ function game:enter()
   self.pet_direction = 1
 
   self.csize = 0
-  self:randomFeatures()
 
   self.ceating = 0
 
   self.cangry = 0
+
+  self._message_dt = 5
+  self._message = "Feed the cat with [A] until it gets off your laptop.\nPet the cat with [L] when it gets angry."
+end
+
+function game:setMessage(msg)
+  self._message = msg
+  self._message_dt = 1
+end
+
+function game:isMessageVisable()
+  return self._message ~= 0
 end
 
 function game:update(dt)
 
-  self.cangry = math.min(1,self.cangry + dt/2)
+  if not self.dead then
+    self._message_dt = math.max(0,self._message_dt - dt)
+  end
 
-  self.food_offset = self.food_offset + dt*self.food_direction*10
+  if not self.death and game.bind.feed() or self.food_direction == -1 then
+    self.food_offset = self.food_offset + dt*self.food_direction*2
+  else
+    self.food_offset = self.food_offset - dt
+  end
 
   if self.food_offset > 1 then
     self.food_offset = 1
     self.food_direction = -1
-    self:randomFeatures()
-    self.ceating = 1
+    if self.ceating == 0 then
+      self:feedCat()
+      self.ceating = 1.5
+      self:setMessage("Om nom nom nom")
+    else
+      self:feedCat()
+      self.cangry = math.min(1,self.cangry + 0.7)
+      self:setMessage("Don't feed the cat when it's mouth is full!")
+    end
   elseif self.food_offset < 0 then
     self.food_offset = 0
     self.food_direction = 1
   end
 
-  self.pet_offset = self.pet_offset + dt*self.pet_direction
+  if not self.death and game.bind.pet() or self.pet_direction == -1 then
+    self.pet_offset = self.pet_offset + dt*self.pet_direction*2
+  else
+    self.pet_offset = self.pet_offset - dt*2
+  end
 
   if self.pet_offset > 1 then
     self.pet_offset = 1
     self.pet_direction = -1
-    self.cangry = 0
+    if self.cangry < 0.1 then
+      self:setMessage("Don't pet the cat when it's not angry!")
+      self.cangry = math.min(1,self.cangry+0.7)
+    else
+      self:setMessage("You calm the cat.")
+      self.cangry = math.max(0,self.cangry-0.2)
+    end
   elseif self.pet_offset < 0 then
     self.pet_offset = 0
     self.pet_direction = 1
+  end
+
+  self.cangry = math.min(1,self.cangry + dt/30)
+
+  if self.csize >= 1 then
+    self.death = true
+    self:setMessage("The cat explodes and kills you.")
+  end
+
+  if self.cangry >= 1 then
+    self.death = true
+    self:setMessage("The cat is so angry it kills you. (Sorry!)")
+  end
+
+
+  if self.death then
+    self.death_fade = math.min(1,self.death_fade + dt)
   end
 
   self.ceating = math.max(0,self.ceating - dt)
@@ -99,10 +158,12 @@ end
 
 function game:draw()
 
-  love.graphics.draw(self.background,0,0,0,2,2)
-  self.atlas:getSprites()['tail_normal.png']:drawFull(0,0,0,2,2)
+  love.graphics.setFont(fonts.default)
 
-  local bi = self.atlas:getSprites()['body.png']
+  love.graphics.draw(self.background,0,0,0,2,2)
+  atlas:getSprites()['tail_normal.png']:drawFull(0,0,0,2,2)
+
+  local bi = atlas:getSprites()['body.png']
   bi:draw(
     256+math.random(-5,5)*self.cangry,
     256+math.random(-5,5)*self.cangry,
@@ -110,39 +171,65 @@ function game:draw()
     bi:getWidth()/2,bi:getHeight()/2
   )
 
-  self.atlas:getSprites()[self.cpaws]:drawFull(0,0,0,2,2)
-  self.atlas:getSprites()[self.ceyes]:drawFull(0,0,0,2,2)
+  atlas:getSprites()[self.cpaws]:drawFull(0,0,0,2,2)
+  atlas:getSprites()[self.ceyes]:drawFull(0,0,0,2,2)
 
   if self.cangry > 0.75 then
-    self.atlas:getSprites()['eyes_brow.png']:drawFull(0,0,0,2,2)
+    atlas:getSprites()['eyes_brow.png']:drawFull(0,0,0,2,2)
   end
-  self.atlas:getSprites()['nose_normal.png']:drawFull(
+  atlas:getSprites()['nose_normal.png']:drawFull(
     self.cangry*math.random(-2,2),
     self.cangry*math.random(-2,2),0,2,2)
 
-  self.atlas:getSprites()[self.cmouth]:drawFull(0,0,0,2,2)
+  atlas:getSprites()[self.cmouth]:drawFull(0,0,0,2,2)
 
   local pet_offset_px = self.tween_pet(self.pet_offset) * 128-128
 
-  self.atlas:getSprites()['hand_upper.png']:drawFull(0,pet_offset_px,0,2,2)
+  atlas:getSprites()['hand_upper.png']:drawFull(0,pet_offset_px,0,2,2)
 
-  local hl = self.atlas:getSprites()['hand_lower.png']
+  local hl = atlas:getSprites()['hand_lower.png']
 
   local food_offset_px = (self.tween_food(self.food_offset)-1)*hl:getWidth()
   if self.food_direction == 1 then
-    self.atlas:getSprites()[self.cfood]:drawFull(food_offset_px,0,0,2,2)
+    atlas:getSprites()[self.cfood]:drawFull(food_offset_px,0,0,2,2)
   end
 
   hl:drawFull(food_offset_px,0,0,2,2)
 
-  if self.csize >= 0.95 then
-    self.atlas:getSprites()['kaboom.png']:drawFull(0,0,0,2,2)
+  if self.csize >= 1 then
+    atlas:getSprites()['kaboom.png']:drawFull(0,0,0,2,2)
+  end
+
+  love.graphics.setColor(
+    colors.death[1],
+    colors.death[2],
+    colors.death[3],
+    self.death_fade*255)
+  love.graphics.rectangle("fill",0,0,512,512)
+  love.graphics.setColor(colors.reset)
+
+  if self._message_dt > 0 then
+    love.graphics.setColor(colors.text_background)
+    local width,lines = fonts.default:getWrap(self._message,512)
+    local height = fonts.default:getHeight()
+    love.graphics.rectangle("fill",0,256+128-8,512,lines*height+16)
+    love.graphics.setColor(colors.text)
+    love.graphics.printf(self._message,0,256+128,512,"center")
+  end
+
+  if self.debug then
+    love.graphics.print("anger:"..self.cangry.."\n"..
+      "size:"..self.csize.."\n"..
+      "message_dt:"..self._message_dt.."\n"..
+      "eating:"..self.ceating,8,8)
   end
 
 end
 
-function game:keypressed()
-  self:init()
+function game:keypressed(key)
+  if key == "`" then
+    self.debug = not self.debug
+  end
 end
 
 return game
